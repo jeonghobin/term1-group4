7b629e536dfffbae28e144140f29470da1aa0688
unknown <ssh5212@naver.com>
2023-01-19 08:58:29 +0900

    230118

---
+++ 신승헌/algorithm/week03/b5_1008/Main.java
@@ -0,0 +1,17 @@
+package Baekjoon.b5_1008;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		
+		System.out.println(a/b);
+	}
+
+}
---
+++ 신승헌/algorithm/week03/b5_2557/Main.java
@@ -0,0 +1,10 @@
+package Baekjoon.b5_2557;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
