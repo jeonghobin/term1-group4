33f594f01d7fbf4b9b1b0f8053e67b55c85c1c64
jeonghobin <64205530+jeonghobin@users.noreply.github.com>
2023-01-27 13:04:15 +0900

    1.27 (다음주 수요일)

--- 정호빈/2week/baek_2493.java
+++ 정호빈/2week/baek_2493.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-
+// 해당 인덱스를 표시하기 위해 idx와 num을 객체로 만듬
 class Node{
 	int idx;
 	int num;
@@ -13,6 +13,12 @@ class Node{
 }
 
 public class Main {
+	
+	// 접근방법 1. 우선 모든 경우의수를 확인한다.  ->  시간초과
+	// 접근방법 2. 규칙을 찾는다. -> 9 5 10 8    9는 5보다 크다 5는 10보다 작다 10은 8보다 크다
+	// 그래서 8은 5 9는 확인할 필요도없다
+	// 그래서 스택을 이용하여 지금까지 탐색한 것중에서 제일 큰 탑이 1번째의 스택에 차있도록하고 뒤에있던 숫자는 신경쓰지않도록한다.
+	// 6 9 5 7 4 ->>    6 -> 9 -> 9 5 -> 9 7
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
