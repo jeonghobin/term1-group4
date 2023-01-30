fdb06d10612a3915a7fda31eeae71ef12a69fab6
jeonghobin <64205530+jeonghobin@users.noreply.github.com>
2023-01-30 20:55:51 +0900

    조합 기본문제

---
+++ 정호빈/3week/baek_11050.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+	static boolean[] visited;
+	static int rs=0;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		visited = new boolean[a];
+		comb(b,a,0,0);
+		System.out.println(rs);
+
+	}
+	
+	public static void comb(int n,int a,int cnt, int start) {
+		if(n==cnt) {
+			rs++;
+			return;
+		}
+		for(int i=start;i<a;i++) {
+			visited[i] = true;
+			comb(n,a,cnt+1,i+1);
+			visited[i] = false;
+		}
+	}
+
+}
