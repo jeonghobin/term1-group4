e234b0aaaebbeb7dbf272606476724519ceded90
jeonghobin <64205530+jeonghobin@users.noreply.github.com>
2023-01-30 21:30:37 +0900

    스택 기본 문제

---
+++ 정호빈/3week/baek_10773.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+
+// 스택을 활용하여 푸는 문제
+// 숫자를 입력하면 스택에 넣고 0을 입력하면 스택을 pop함
+
+public class Main {
+	
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		Stack<Integer> st = new Stack<Integer>();
+		int N = Integer.parseInt(br.readLine());
+		for(int i=0;i<N;i++) {
+			int a = Integer.parseInt(br.readLine());
+			if(a!=0) {
+				st.push(a);
+			}
+			else {
+				st.pop();
+			}
+		}
+		
+		int rs =0;
+		while(!st.empty()) {
+			rs+=st.pop();
+		}
+		System.out.println(rs);
+		
+	}
+	
+	
+}
