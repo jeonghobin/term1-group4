8165db995cec91dea9a61f68912fc953296a6728
bmsnc <qjatjs98@gmail.com>
2023-02-06 21:06:07 +0900

    02/06

--- 최범선/src/알고리즘/Swea/Swea_1289.java
+++ 최범선/src/알고리즘/Swea/Swea_1289.java
@@ -3,6 +3,47 @@ package 알고리즘.Swea;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import java.util.StringTokenizer;
+
+//public class Swea_1289 {
+//
+//    static int cnt;
+//    static String a;
+//
+//    public static void main(String[] args) throws Exception {
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st;
+//        int tc = Integer.parseInt(br.readLine());
+//
+//        for (int t = 1; t < tc + 1; t++) {
+//            StringBuilder sb = new StringBuilder();
+//            sb.append("#").append(t).append(" ");
+//            a = br.readLine();
+//
+//            recursive(0, 1);
+//
+//            sb.append(cnt);
+//            cnt = 0;
+//            System.out.println(sb);
+//        }
+//    }
+//
+//    private static void recursive(int idx , int state) {
+//        if (idx >= a.length()) return;
+//
+//        if (a.charAt(idx) == state + '0' ) {
+//            state++;
+//            state %= 2;
+//            cnt++;
+//        }
+//        recursive(idx+1, state);
+//
+//    }
+//}
+
+
+
 public class Swea_1289 {
 
     public static void main(String[] args) throws Exception {
