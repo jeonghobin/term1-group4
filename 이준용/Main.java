1187c16b643f9a2fce18e503792b53304047474f
elle6044 <123047819+elle6044@users.noreply.github.com>
2023-02-07 13:48:57 +0900

    재귀-별찍기-19

--- 이준용/Main.java
+++ 이준용/Main.java
@@ -1,19 +1,34 @@
-package backalgo;
+package baek;
 
 import java.io.*;
 import java.util.*;
-
 public class Main {
-	public static void main(String[] args) throws IOException{
+
+	static int max;
+	static char[] list= {'*',' '};
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st2=new StringTokenizer(br.readLine());
-		StringTokenizer st=new StringTokenizer(br.readLine());
-		int num=Integer.parseInt(st2.nextToken())*Integer.parseInt(st2.nextToken());
-		for(int i=0;i<5;i++) {
-			System.out.print(Integer.parseInt(st.nextToken())-num+" ");
+		
+		max=Integer.parseInt(br.readLine());
+		recursive(0,"");
+	}
+	private static void recursive(int n, String star) {
+		String newstar="";
+		for(int i=0;i<(4*(max-1)+1-2*n);i++) {
+			newstar+=list[n%2];
+		}
+		if(n!=0) {
+			newstar=star.substring(0, n)+newstar+star.substring(star.length()-n, star.length());
 		}
 		
+		if(n==2*(max-1)) {
+			System.out.println(newstar);
+			return;
+		}
+		System.out.println(newstar);
+		recursive(n+1,newstar);
+		System.out.println(newstar);
 	}
 }
