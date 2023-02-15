a733d9d90767e9cc0db328877c01bc28518ca8d3
AngelPlayer <ssh5212@naver.com>
2023-02-15 22:11:26 +0900

    0213 - 0215

---
+++ 신승헌/algorithm/SWEA/d_1952/Solution.java
@@ -0,0 +1,28 @@
+package algo.swea.d_1952;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	static int day, month, monthThree, year;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int T = Integer.parseInt(st.nextToken());
+		
+		for(int test_case = 1; test_case <= T; test_case++) {
+			st = new StringTokenizer(br.readLine());
+			
+			day = Integer.parseInt(st.nextToken());
+			month = Integer.parseInt(st.nextToken());
+			monthThree = Integer.parseInt(st.nextToken());
+			year = Integer.parseInt(st.nextToken());
+			
+			recursive(0, )
+			
+			
+		} // [E] test_case
+	}
+}
