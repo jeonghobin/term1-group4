f124476f170e02ee8283aceee26dbf55913d7406
somyeong <gkathaud@naver.com>
2023-02-24 01:38:14 +0900

    02.23

--- 함소명/src/com/ssafy/study/beakjoon/beak_1697/Main.java
+++ 함소명/src/com/ssafy/study/beakjoon/beak_1697/Main.java
@@ -1,10 +1,70 @@
 package com.ssafy.study.beakjoon.beak_1697;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/*
+ * 숨바꼭질
+ * 큐에 해당 위치를 넣어주고, 해당 숫자를 인덱스로 방문 체크.
+ */
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static boolean[] check = new boolean[100001];// 100000까지 들어감.
+	static int cnt;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());// 수빈이 위치
+		int M = Integer.parseInt(st.nextToken());// 동생 위치
+		int[] arr = new int[100000];
+		cnt = 0;
+
+		if (N == M)
+			System.out.println(0);
+		else
+			BFS(N, M);
 
 	}
 
+	private static void BFS(int N, int M) {
+		Queue<Integer> Q = new LinkedList<>();
+
+		Q.offer(N);
+		check[N] = true;
+
+		L: while (!Q.isEmpty()) {
+
+			cnt++;
+
+			int len = Q.size();
+			for (int i = 0; i < len; i++) {
+				int num = Q.poll();
+				for (int j = 0; j < 3; j++) {
+					int move = 0;
+					if (j == 0)
+						move = num + 1;
+					if (j == 1)
+						move = num - 1;
+					if (j == 2)
+						move = num * 2;
+
+					if (move >= 0 && move <= 100000 && !check[move]) {
+						check[move] = true;
+						Q.offer(move);
+					}
+				}
+				if (check[M]) {
+					System.out.println(cnt);
+
+					break L;
+				}
+			}
+		}
+
+	}
 }
--- 함소명/src/com/ssafy/study/swea/탈주범검거_1953.java
+++ 함소명/src/com/ssafy/study/swea/탈주범검거_1953.java
@@ -31,17 +31,19 @@ public class 탈주범검거_1953 {
 
 	static class Position {
 		int r, c, pipe;
+		List<Integer> dir = new ArrayList<>();
 
 		@Override
 		public String toString() {
-			return "Position [r=" + r + ", c=" + c + ", pipe=" + pipe + "]";
+			return "Position [r=" + r + ", c=" + c + ", pipe=" + pipe + ", dir=" + dir + "]";
 		}
 
-		public Position(int r, int c, int pipe) {
+		public Position(int r, int c, int pipe, List dir) {
 			super();
 			this.r = r;
 			this.c = c;
 			this.pipe = pipe;
+			this.dir = dir;
 		}
 	}
 
@@ -56,7 +58,7 @@ public class 탈주범검거_1953 {
 			int R = Integer.parseInt(st.nextToken());
 			int C = Integer.parseInt(st.nextToken());
 			L = Integer.parseInt(st.nextToken());// 탈출 후 소요된 시간
-			cnt = 1;
+			cnt = 0;
 
 			map = new int[N][M];
 			visit = new boolean[N][M];
@@ -76,8 +78,9 @@ public class 탈주범검거_1953 {
 		Queue<Position> Q = new LinkedList<>();
 
 		int pipe = map[r][c];
-		Q.add(new Position(r, c, map[r][c]));
-//		Position p1 = Q.peek();
+		// x,y,pipe,파이프가 갈 수 있는 방향
+		Q.offer(new Position(r, c, pipe, PipeDir(pipe)));
+
 //		System.out.println(p1.toString());
 
 		L: while (!Q.isEmpty()) {
@@ -85,148 +88,78 @@ public class 탈주범검거_1953 {
 			int len = Q.size();
 			for (int i = 0; i < len; i++) {
 				Position p = Q.poll();
+				visit[r][c] = true;
+				cnt++;
+//				System.out.println(p.toString());
 
-				switch (p.pipe) {
-				case 1:
-					for (int j = 0; j < 4; j++) {
-						int dx = p.r + dr[j];
-						int dy = p.c + dc[j];
+				// 갈 수 있는 방향
+				for (Integer dir2 : p.dir) {
+					// 방향이 들어왔을 때 갈 수 있는 파이프 리턴
+					List list = findPipe(dir2);
 
-						if (dx >= 0 && dy >= 0 && dx < N && dy < M && map[dx][dy] != 0 && !visit[dx][dy]) {
-							visit[p.r][p.c] = true;
-							cnt++;
-							Q.offer(new Position(dx, dy, map[dx][dy]));
-						}
-					}
-					break;
-				case 2:
-					for (int j = 0; j < 4; j = j + 2) {
-						// 상=0, 하=2
-						int dx = p.r + dr[j];
-						int dy = p.c + dc[j];
+					for (int j = 0; j < list.size(); j++) {
+						// 들어온 파이프가 갈 수 있는 파이프라면 이동
+
+//						for (Integer dir : p.dir) {
+						int dx = p.r + dr[dir2];
+						int dy = p.c + dc[dir2];
 
 						if (dx >= 0 && dy >= 0 && dx < N && dy < M && map[dx][dy] != 0 && !visit[dx][dy]) {
-							if (map[dx][dy] == 1 || map[dx][dy] == 2|| map[dx][dy] == 4 || map[dx][dy] == 7) {
-								visit[p.r][p.c] = true;
-								cnt++;
-								pipe = map[dx][dy];
-								Q.offer(new Position(dx, dy, pipe));
+							if ((int) list.get(j) == map[dx][dy]) {
+								visit[dx][dy] = true;
+
+								Q.offer(new Position(dx, dy, map[dx][dy], PipeDir(map[dx][dy])));
 							}
 						}
+//						}
 					}
-					break;
-				case 3:
-					for (int j = 1; j < 4; j = j + 2) {
-						// 좌=1, 우=3
-						int dx = p.r + dr[j];
-						int dy = p.c + dc[j];
-
-						if (dx >= 0 && dy >= 0 && dx < N && dy < M && map[dx][dy] != 0 && !visit[dx][dy]) {
-							if (map[dx][dy] == 1 || map[dx][dy] == 3) {
-								visit[p.r][p.c] = true;
-								cnt++;
-								pipe = map[dx][dy];
-								Q.offer(new Position(dx, dy, pipe));
-							}
-						}
-					}
-					break;
-				case 4:
-					for (int j = 0; j < 2; j++) {
-						// 상=0 우=1 -> 하=2 좌=3 에서 들어와야함.
-						int dx = p.r + dr[j];
-						int dy = p.c + dc[j];
-
-						if (dx >= 0 && dy >= 0 && dx < N && dy < M && map[dx][dy] != 0 && !visit[dx][dy]) {
-							if (map[dx][dy] == 1 || map[dx][dy] == 0) {
-								visit[p.r][p.c] = true;
-								cnt++;
-								pipe = map[dx][dy];
-								Q.offer(new Position(dx, dy, pipe));
-							}
-						}
-					}
-					break;
-				case 5:
-					for (int j = 1; j < 3; j++) {
-						// 우=1 하=2-> 좌=3 상=0 에서 들어와야함.
-						int dx = p.r + dr[j];
-						int dy = p.c + dc[j];
-
-						if (dx >= 0 && dy >= 0 && dx < N && dy < M && map[dx][dy] != 0 && !visit[dx][dy]) {
-							if (map[dx][dy] == 3 || map[dx][dy] == 0) {
-								visit[p.r][p.c] = true;
-								cnt++;
-								pipe = map[dx][dy];
-								Q.offer(new Position(dx, dy, pipe));
-							}
-						}
-					}
-					break;
-				case 6:
-					for (int j = 2; j < 4; j++) {
-						// 하=2 좌=3-> 상=0 우=1 에서 들어와야함.
-						int dx = p.r + dr[j];
-						int dy = p.c + dc[j];
-
-						if (dx >= 0 && dy >= 0 && dx < N && dy < M && map[dx][dy] != 0 && !visit[dx][dy]) {
-							if (map[dx][dy] == 0 || map[dx][dy] == 1) {
-								visit[p.r][p.c] = true;
-								cnt++;
-								pipe = map[dx][dy];
-								Q.offer(new Position(dx, dy, pipe));
-							}
-						}
-					}
-					break;
-				case 7:
-					for (int j = 0; j < 4; j = j + 3) {
-						// 상=0, 좌=3-> 우=1 하=2 에서 들어와야함.
-						int dx = p.r + dr[j];
-						int dy = p.c + dc[j];
-
-						if (dx >= 0 && dy >= 0 && dx < N && dy < M && map[dx][dy] != 0 && !visit[dx][dy]) {
-							if (map[dx][dy] == 1 || map[dx][dy] == 2) {
-								visit[p.r][p.c] = true;
-								cnt++;
-								pipe = map[dx][dy];
-								Q.offer(new Position(dx, dy, pipe));
-							}
-						}
-					}
-					break;
-
-				default:
-					break;
 				}
 			}
 
 			L--;
 
-			if (L == 1)
+			if (L == 0)
 				break L;
 		}
 
 	}
 
-//	public static List Fpipe(int pipe) {
-//		List<Integer> list = new ArrayList<>();
-//
-//		if (pipe == 1)
-//			list = List.of(0, 1, 2, 3);
-//		else if (pipe == 2)
-//			list = List.of(0, 2);
-//		else if (pipe == 3)
-//			list = List.of(1, 3);
-//		else if (pipe == 4)
-//			list = List.of(0, 1);
-//		else if (pipe == 5)
-//			list = List.of(1, 2);
-//		else if (pipe == 6)
-//			list = List.of(2, 3);
-//		else if (pipe == 7)
-//			list = List.of(3, 0);
-//
-//		return list;
-//	}
+	// 파이프가 갈 수 있는 방향 리턴
+	public static List<Integer> PipeDir(int pipe) {
+		List<Integer> list = new ArrayList<>();
+
+		if (pipe == 1)
+			list = Arrays.asList(0, 1, 2, 3);
+		else if (pipe == 2)
+			list = Arrays.asList(0, 2);
+		else if (pipe == 3)
+			list = Arrays.asList(1, 3);
+		else if (pipe == 4)
+			list = Arrays.asList(0, 1);
+		else if (pipe == 5)
+			list = Arrays.asList(1, 2);
+		else if (pipe == 6)
+			list = Arrays.asList(2, 3);
+		else if (pipe == 7)
+			list = Arrays.asList(3, 0);
+
+		return list;
+	}
+
+	// 방향이 들어왔을 때 갈 수 있는 파이프 리턴
+	public static List<Integer> findPipe(int dir) {
+		List<Integer> list = new ArrayList<>();
+
+		if (dir == 0)// 상
+			list = Arrays.asList(1, 2, 5, 6);
+		if (dir == 1)// 우
+			list = Arrays.asList(1, 3, 6, 7);
+		if (dir == 2)// 하
+			list = Arrays.asList(1, 2, 4, 7);
+
+		if (dir == 3)// 좌
+			list = Arrays.asList(1, 3, 4, 5);
+
+		return list;
+	}
 }
