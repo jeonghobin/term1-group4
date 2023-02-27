7d096df3e3d25901493f8a3f2cc904f28bb899fc
bmsnc <qjatjs98@gmail.com>
2023-02-28 02:02:14 +0900

    02/28

--- 최범선/src/알고리즘/Swea/Swea_1868.java
+++ 최범선/src/알고리즘/Swea/Swea_1868.java
@@ -5,58 +5,52 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-
 public class Swea_1868 {
-
-    static char[][] data;
+    static int N;
+    static char[][] c;
     static int[][] map;
-    static boolean[][] v;
-    static int N, cnt;
     static int[] dx = {0,1,0,-1,1,1,-1,-1};
     static int[] dy = {1,0,-1,0,1,-1,-1,1};
-
-    public static class Position{
+    public static class Pair{
         int x, y;
-        public Position(int x, int y) {
+        public Pair(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
-    public static void bfs(int x, int y){
-        Queue<Position> q = new LinkedList<>();
-        v = new boolean[N][N];
-        q.add(new Position(x, y));
-        v[x][y] = true;
-        map[x][y] = 1;
-
-        while (!q.isEmpty()){
-            Position p = q.poll();
-            if (check(p.x, p.y)) map[p.x][p.y] = 1;
-            else{
-            for (int i = 0; i < 8; i++) {
-                int nx = p.x + dx[i];
-                int ny = p.y + dy[i];
-                if (nx >= 0 && nx < N && ny >= 0 && ny < N){
-                    if (!v[nx][ny] && data[nx][ny] == '.' ){
-                        if (map[nx][ny] > 0 && map[nx][ny] != 9) cnt--;
-                        map[nx][ny] = 0;
-                        v[nx][ny] = true;
-                        if (!check(nx, ny)) q.add(new Position(nx, ny));
-                    }
-                }
-                }
-            }
-        }
-    }
-    public static boolean check(int x, int y){
+    public static int check(int x, int y){
+        int cnt = 0;
         for (int i = 0; i < 8; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if (nx >= 0 && nx < N && ny >= 0 && ny < N){
-                if (data[nx][ny] == '*') return true;
+            if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+                if(c[nx][ny] == '*') cnt++;
             }
         }
-        return false;
+        return cnt;
+    }
+    public static void bfs(int a, int b){
+        map[a][b] = 0;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(a, b));
+
+        while (!q.isEmpty()){
+            Pair t = q.poll();
+            int x = t.x;
+            int y = t.y;
+            for (int i = 0; i < 8; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx >= 0 && nx < N && ny >= 0 && ny < N){
+                    if(map[nx][ny] != 0 && c[nx][ny] == '.'){
+                        if (check(nx, ny) == 0) q.add(new Pair(nx, ny));
+                        map[nx][ny] = 0;
+                    }
+                }
+            }
+        }
+
+
     }
 
     public static void main(String[] args) throws IOException {
@@ -66,26 +60,29 @@ public class Swea_1868 {
         StringBuilder sb = new StringBuilder();
 
         for (int t = 1; t < tc + 1; t++) {
-
             sb.append("#").append(t).append(" ");
             N = Integer.parseInt(br.readLine());
-            data = new char[N][N];
+            c = new char[N][N];
             map = new int[N][N];
-
             for (int i = 0; i < N; i++) {
                 String a = br.readLine();
                 for (int j = 0; j < N; j++) {
-                    data[i][j] = a.charAt(j);
-                    map[i][j] = 9;
+                    c[i][j] = a.charAt(j);
+                    map[i][j] = -1;
                 }
             }
-            cnt = 0;
+            int cnt = 0;
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    if (data[i][j] == '.' && map[i][j] != 0){
+                    if(c[i][j] == '.' && check(i,j) == 0 && map[i][j] != 0){
                         bfs(i, j);
                         cnt++;
                     }
+                }
+            }
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if(map[i][j] != 0 && c[i][j] == '.') cnt++;
                 }
             }
             sb.append(cnt).append("\n");
