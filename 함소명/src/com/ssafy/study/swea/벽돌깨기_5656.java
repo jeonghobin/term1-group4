49bea499b325e0c751b9ecfb36f80a7053f054aa
somyeong <gkathaud@naver.com>
2023-03-06 08:24:21 +0900

    03.05

--- 함소명/src/com/ssafy/study/swea/벽돌깨기_5656.java
+++ 함소명/src/com/ssafy/study/swea/벽돌깨기_5656.java
@@ -1,22 +1,16 @@
 package com.ssafy.study.swea;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 /*
  * 구술을 쏘아 벽돌을 깨트리는 게임
- * 구슬은 좌, 우로만 움직일 수 있어서 항상 맨 위에 있는 벽돌만 깨트릴 수 있다.
- * 벽돌은 숫자 1 ~ 9 로 표현되며, 구술이 명중한 벽돌은 상하좌우로 ( 벽돌에 적힌 숫자 - 1 ) 칸 만큼 같이 제거된다.
- * 제거되는 범위 내에 있는 벽돌은 동시에 제거된다.
+ * 구슬을 떨어뜨리는 곳을 먼저 중복 순열로 뽑는 다는것만 캐치하면 됨.
  */
 public class 벽돌깨기_5656 {
 	static int N, W, H;
 	static int[][] map;
-
+	static int min = Integer.MAX_VALUE;
 	static int[] dr = { -1, 1, 0, 0 };
 	static int[] dc = { 0, 0, -1, 1 };
 
@@ -46,6 +40,7 @@ public class 벽돌깨기_5656 {
 			W = Integer.parseInt(st.nextToken());// 넓이
 			H = Integer.parseInt(st.nextToken());// 높이
 			map = new int[H][W];
+			min = Integer.MAX_VALUE;
 
 			for (int i = 0; i < H; i++) {
 				st = new StringTokenizer(br.readLine());
@@ -54,10 +49,127 @@ public class 벽돌깨기_5656 {
 				}
 			}
 
-			findVertex(map);
+			select(new int[N], 0);
 
-			print(map);
+			System.out.println("#" + test_case + " " + min);
+//			print(map);
+		}
+	}
 
+	// 공 떨어뜨리는 곳 순서 정함.
+	// 중복 순열 : 뿌시는 순서에 따라 달라짐.
+	private static void select(int[] sel, int idx) {
+		if (idx == N) {
+//			System.out.println(Arrays.toString(sel));
+			gameMap(sel);
+
+			return;
+		}
+
+		for (int i = 0; i < W; i++) {
+			sel[idx] = i;
+			select(sel, idx + 1);
+		}
+	}
+
+	static int[][] gameBoard;// N판 게임마다
+
+	private static void gameMap(int[] sel) {
+		gameBoard = new int[H][W];
+		for (int j = 0; j < H; j++) {
+			for (int i = 0; i < W; i++) {
+				gameBoard[j][i] = map[j][i];
+			}
+		}
+
+		for (int i = 0; i < sel.length; i++) {
+			// 구슬이 떨어지는 좌표
+			int c = sel[i];
+			int r = 0;
+			for (int j = 0; j < H; j++) {
+				if (gameBoard[j][c] != 0) {
+					r = j;
+					break;
+				}
+			}
+
+			// 제일 위칸이 0이면 깰 블록이 없음.
+			if (gameBoard[r][c] != 0) {
+
+				blockbreak(r, c, gameBoard[r][c]);
+
+				blockdown();
+			}
+		}
+
+//		print(gameBoard);
+
+		int sumCnt = 0;
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < W; j++) {
+				if (gameBoard[i][j] != 0)
+					sumCnt++;
+			}
+		}
+
+		min = Math.min(min, sumCnt);
+	}
+
+	// 벽돌 부수기
+	private static void blockbreak(int r, int c, int num) {
+
+		Queue<Point> Q = new LinkedList<>();
+		Q.add(new Point(r, c, num));
+
+		while (!Q.isEmpty()) {
+			Point p = Q.poll();
+
+			gameBoard[p.r][p.c] = 0;
+
+			for (int i = 0; i < 4; i++) {
+				// 퍼져나가는 깊이
+				for (int depth = 1; depth < p.cnt; depth++) {
+					int dx = p.r + (dr[i] * depth);
+					int dy = p.c + (dc[i] * depth);
+
+					if (dx >= 0 && dy >= 0 && dx < H && dy < W && gameBoard[dx][dy] != 0) {
+						if (gameBoard[dx][dy] != 1)
+							Q.add(new Point(dx, dy, gameBoard[dx][dy]));
+						gameBoard[dx][dy] = 0;
+					}
+				}
+			}
+		}
+	}
+
+	// 벽돌 내리기
+	private static void blockdown() {
+		Stack<Integer>[] stacks = new Stack[W];
+		for (int i = 0; i < W; i++) {
+			stacks[i] = new Stack();
+		}
+
+		// 0이 아니면 리스트 담기
+		for (int j = 0; j < H; j++) {
+			for (int i = 0; i < W; i++) {
+				if (gameBoard[j][i] != 0)
+					stacks[i].push(gameBoard[j][i]);
+			}
+		}
+
+		// 0으로 초기화
+		for (int j = 0; j < H; j++) {
+			for (int i = 0; i < W; i++) {
+				gameBoard[j][i] = 0;
+			}
+		}
+
+		// 순서대로 다시 넣기.
+		for (int j = 0; j < W; j++) {
+			int size = stacks[j].size();
+			for (int i = 0; i < size; i++) {
+				gameBoard[(H - 1) - i][j] = stacks[j].pop();
+			}
 		}
 	}
 
@@ -68,57 +180,7 @@ public class 벽돌깨기_5656 {
 			}
 			System.out.println();
 		}
-	}
-
-
-	static int min = Integer.MAX_VALUE;
-
-	private static void findVertex(int[][] board) {
-		boolean[] v = new boolean[W];
-
-		// 구슬이 깰 수 있는 맨 위 정점 넣기.
-		for (int i = 0; i < H; i++) {
-			for (int j = 0; j < W; j++) {
-				if (!v[j] && board[i][j] != 0) {
-					v[j] = true;
-		
-					BFS(new Point(i, j, board[i][j]), board,0);
-				}
-			}
-		}
-		
-	}
-
-	private static void BFS(Point start, int[][] board, int k) {
-
-		int sum = 0;
-		
-		Queue<Point> Q = new LinkedList<벽돌깨기_5656.Point>();
-		Q.add(start);
-
-		if (!Q.isEmpty()) {
-			Point p = Q.poll();
-
-			for (int i = 0; i < 4; i++) {
-
-				// 퍼져나가는 깊이
-				for (int depth = 1; depth < p.cnt; depth++) {
-					int dx = p.r + dr[i] + depth;
-					int dy = p.c + dc[i] + depth;
-
-					if (dx >= 0 && dy >= 0 && dx < H && dy < W) {
-						sum += board[dx][dy];
-						if (board[dx][dy] > 1)
-							Q.add(new Point(dx, dy, board[dx][dy]));
-					}
-				}
-			}
-		}
-
-		findVertex(board);
-		
-		System.out.println(Q.toString());
-
+		System.out.println();
 	}
 
 }
--- 함소명/src/com/ssafy/study/swea/최소스패닝트리_D4_3124/프림_PQ.java
+++ 함소명/src/com/ssafy/study/swea/최소스패닝트리_D4_3124/프림_PQ.java
@@ -1,19 +1,13 @@
 package com.ssafy.study.swea.최소스패닝트리_D4_3124;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 
 public class 프림_PQ {
 	static int V;
 	static ArrayList<Vertex>[] adj;
 
-	static class Vertex implements Comparable<Vertex>{
+	static class Vertex implements Comparable<Vertex> {
 		int e, c;
 
 		Vertex(int e, int c) {
@@ -31,7 +25,7 @@ public class 프림_PQ {
 			// TODO Auto-generated method stub
 			return Integer.compare(this.c, o.c);
 		}
-		
+
 	}
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -43,7 +37,7 @@ public class 프림_PQ {
 			int E = Integer.parseInt(st.nextToken());
 
 			// 인접리스트
-			adj = new ArrayList[V+1];
+			adj = new ArrayList[V + 1];
 			for (int i = 1; i <= V; i++) {
 				adj[i] = new ArrayList<Vertex>();
 			}
@@ -58,7 +52,7 @@ public class 프림_PQ {
 				adj[b].add(new Vertex(a, c));
 			}
 
-			System.out.println("#"+test_case+" "+primPQ(new int[V + 1], new boolean[V + 1]));
+			System.out.println("#" + test_case + " " + primPQ(new int[V + 1], new boolean[V + 1]));
 
 		}
 
@@ -71,14 +65,14 @@ public class 프림_PQ {
 		/***** 정점배열의 값을 무한대로 설정 *****/
 		Arrays.fill(Node, Integer.MAX_VALUE);
 		Node[0] = 0;
-		
-		long sum  = 0;
+
+		long sum = 0;
 		PriorityQueue<Vertex> Q = new PriorityQueue<>();
 		Q.add(new Vertex(1, 0));
-		
+
 		while (!Q.isEmpty()) {
 			Vertex p = Q.poll();
-			
+
 			if (!v[p.e]) {
 				v[p.e] = true;
 
@@ -93,7 +87,7 @@ public class 프림_PQ {
 				}
 			}
 		}
-		
+		System.out.println(Arrays.toString(Node));
 		return sum;
 	}
 
