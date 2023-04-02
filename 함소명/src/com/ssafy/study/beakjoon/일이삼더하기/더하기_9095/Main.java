20279f452b5a90b6850bd51e83585f8130f5c2b0
somyeong <gkathaud@naver.com>
2023-04-03 01:45:27 +0900

    04.02

--- 함소명/src/com/ssafy/study/beakjoon/일이삼더하기/더하기_9095/Main.java
+++ 함소명/src/com/ssafy/study/beakjoon/일이삼더하기/더하기_9095/Main.java
@@ -3,59 +3,97 @@ package com.ssafy.study.beakjoon.일이삼더하기.더하기_9095;
 import java.util.*;
 
 public class Main {
+
+	static int N, Ans;
 	static int[] dp;
-	static int Ans;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 
-		for (int i = 0; i < T; i++) {
+		for (int t = 0; t < T; t++) {
+			N = sc.nextInt();
 			Ans = 0;
-			int N = sc.nextInt();
-			
-			//완탐
-			recursive(N, 0, 0);
-			
-			// 1, 2, 3의 합으로 나타내는 방법의 수
-			dp = new int[12];
-			dp[1] = 1;
-			dp[2] = 2;
-			dp[3] = 4;
 
-			// 하향식 DP
-			int Ans = recursive_memo(N);
+//			// 백트래킹
+//			backtracking(0);
 
+			// 백트래킹 리턴
+//			Ans = returnback(0);
+
+//			// DP
+//			dp = new int[N+1];
+//			Arrays.fill(dp, -1);
+//			Ans = recursive(0);
+			
+			//점화식
+	        dp[1] = 1;
+	        dp[2] = 2;
+	        dp[3] = 4;
+	        for (int i = 4; i <= N; i++) {
+	            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+	        }
+	        Ans = dp[N];
+
+			
 			System.out.println(Ans);
 		}
-
 	}
 
-	private static int recursive_memo(int n) {
-		if (n == 1)
-			return 0;
-		if (n == 2)
-			return 0;
-		if (n == 3)
-			return 0;
-
-		if (dp[n] != 0)
-			return dp[n];
-
-		return dp[n] = recursive_memo(dp[n - 1]) + recursive_memo(dp[n - 2]);
-	}
-
-	private static void recursive(int M, int k, int sum) {
-		if (sum > M)
+	private static void backtracking(int n) {
+		if (n > N)
 			return;
-		if (sum == M) {
+
+		if (n == N) {
 			Ans++;
 			return;
 		}
 
 		for (int i = 1; i <= 3; i++) {
-			recursive(M, i, sum + i);
+			backtracking(n + i);
 		}
+	}
+
+	private static int returnback(int n) {
+		if (n > N)
+			return 0;
+
+		if (n == N) {
+			return 1;
+		}
+
+		Ans = 0;//맨 끝에서부터 리턴값을 받아야하니까
+		for (int i = 1; i <= 3; i++) {
+			Ans += returnback(n + i);
+		}
+		 
+		return Ans;
+	}
+	
+	
+	//dp 배열은 재귀 인자와 같게 한다.
+	//dp[2] = 2;
+	//dp[3] = 4;
+	private static int recursive(int n) {
+		if (n > N)
+			return 0;
+
+		if (n == N) {
+			return 1;
+		}
+		
+		if(dp[n]!=-1) return dp[n];
+
+		Ans = 0;//맨 끝에서부터 리턴값을 받아야하니까
+		//n이 될 수 있는 경우 수 리턴
+		//24, 13, 7, 4, 2, 1
+		for (int i = 1; i <= 3; i++) {
+			Ans += recursive(n + i);
+		}
+		
+		dp[n] = Ans;
+		 
+		return dp[n];
 	}
 
 }
