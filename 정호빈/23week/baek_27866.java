24b486185331e0c08f05d4eff35f756afc77759d
jeonghobin <64205530+jeonghobin@users.noreply.github.com>
2023-07-03 23:32:27 +0900

    7.03

---
+++ 정호빈/23week/baek_27866.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+
+
+
+public class Main {
+	
+	
+  	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		String str = br.readLine();
+		int a = Integer.parseInt(br.readLine());
+		System.out.println(str.charAt(a-1));
+		
+	}
+
+
+ 	
+
+}
